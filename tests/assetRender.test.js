0a3263950246dcceeb3a1c716ba9471db90930a1
coreyakacorey <48829912+coreyakacorey@users.noreply.github.com>
2024-11-30 15:16:21 +0900

    Updated the assetRender test

--- tests/assetRender.test.js
+++ tests/assetRender.test.js
@@ -29,32 +29,84 @@ describe('AR Web App', () => {
     await browser.close();
   });
 
-  test('Asteroid and spacecraft objects are rendered on screen', async () => {
+  test('Asteroid object and orbits are rendered on screen', async () => {
     
     //Check is asteroid entity exists
     const asteroidExists = await page.$('#psyche');
     expect(asteroidExists).not.toBeNull();
 
+    const orbitDExists = await page.$('#orbitD');
+    expect(asteroidExists).not.toBeNull();
+
+    const orbitCExists = await page.$('#orbitC');
+    expect(asteroidExists).not.toBeNull();
+
+    const orbitBExists = await page.$('#orbitB');
+    expect(asteroidExists).not.toBeNull();
+
+    const orbitAExists = await page.$('#orbitA');
+    expect(asteroidExists).not.toBeNull();
+
     //Ensure the model has been loaded using the `model-loaded` event
-    const modelLoaded = await page.evaluate(() => {
-        return new Promise((resolve) => {
-          const asteroid = document.querySelector('#psyche');
-          if (!asteroid) resolve(false);
-  
-          //Listen for the 'model-loaded' event
-          asteroid.addEventListener('model-loaded', () => resolve(true));
-        });
+    const asteroidLoaded = await page.evaluate(() => {
+      return new Promise((resolve) => {
+        const asteroid = document.querySelector('#psyche');
+        if (!asteroid) resolve(false);
+
+        //Listen for the 'model-loaded' event
+        asteroid.addEventListener('model-loaded', () => resolve(true));
       });
+    });
 
     //Check if asteroid is visible
     const isAsteroidVisible = await page.evaluate(() => {
-        const asteroid = document.querySelector('#psyche');
-        if(!asteroid) return false;
+      const asteroid = document.querySelector('#psyche');
+      if(!asteroid) return false;
 
-        const isVisible = asteroid.getAttribute('visible') !== 'false';
-        return isVisible;
+      const isVisible = asteroid.getAttribute('visible') !== 'false';
+      return isVisible;
     });
     expect(isAsteroidVisible).toBe(true);  // Ensure it's visible
+
+    //Check if orbit D is visible
+    const isOrbitDVisible = await page.evaluate(() => {
+      const orbitD = document.querySelector('#orbitD');
+      if(!orbitD) return false;
+
+      const isVisible = orbitD.getAttribute('visible') !== 'false';
+      return isVisible;
+    });
+    expect(isOrbitDVisible).toBe(true);  // Ensure it's visible
+
+    //Check if orbit D is visible
+    const isOrbitCVisible = await page.evaluate(() => {
+      const orbitC = document.querySelector('#orbitC');
+      if(!orbitC) return false;
+
+      const isVisible = orbitC.getAttribute('visible') !== 'false';
+      return isVisible;
+    });
+    expect(isOrbitCVisible).toBe(true);  // Ensure it's visible
+
+    //Check if orbit D is visible
+    const isOrbitBVisible = await page.evaluate(() => {
+      const orbitB = document.querySelector('#orbitB');
+      if(!orbitB) return false;
+
+      const isVisible = orbitB.getAttribute('visible') !== 'false';
+      return isVisible;
+    });
+    expect(isOrbitBVisible).toBe(true);  // Ensure it's visible
+
+    //Check if orbit D is visible
+    const isOrbitAVisible = await page.evaluate(() => {
+      const orbitA = document.querySelector('#orbitA');
+      if(!orbitA) return false;
+
+      const isVisible = orbitA.getAttribute('visible') !== 'false';
+      return isVisible;
+    });
+    expect(isOrbitAVisible).toBe(true);  // Ensure it's visible
 
   }, 10000);
 });
